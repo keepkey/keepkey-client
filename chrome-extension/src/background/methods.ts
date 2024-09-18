7699e5ec81f1e7ec40e1041fb2d34de33cccaa2e
bithighlander <bithighlander@gmail.com>
2024-09-18 15:08:07 -0700

    broke pairing, dont know why

--- chrome-extension/src/background/methods.ts
+++ chrome-extension/src/background/methods.ts
@@ -106,7 +106,7 @@ const requireApproval = async function (requestInfo: any, chain: any, method: st
     } else {
       throw new Error('Event not saved');
     }
-    openPopup();
+    // openPopup();
   } catch (e) {
     console.error(tag, e);
   }
